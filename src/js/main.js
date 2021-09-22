7f1aa2f56619de7e3f66fa0a098a667e9b73ad37
Iskandarov Timur <timur.iskandarov96@gmail.com>
2021-09-22 11:06:02 +0600

    Добавляет тег "Delta Squad" в консоль разработчика

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,18 @@ import '../css/style.css';
 document.querySelector('#app').innerHTML = `
   <h1>Hello, Timur!</h1>
 `;
+
+const consoleStyles = `
+    color:#fff;
+    background-image: linear-gradient(to left,
+        #dd2a2a, #c61278, #a53195, #7645a7,
+        #5f4faf, #4258b5, #0f5fb7, #1767c0,
+        #2478d3, #2a81dd
+        );
+    padding: 8px 15px;
+    font-size: 14px;
+    border-radius: 4px;
+    text-align:center
+`;
+
+console.log('%cKolesa Upgrade - Delta Squad', consoleStyles);
