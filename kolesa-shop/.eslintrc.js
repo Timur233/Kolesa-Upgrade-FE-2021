c993c94160f27e5b111e6c357a12832c27c97775
Iskandarov Timur <timur.iskandarov96@gmail.com>
2021-10-21 11:59:25 +0600

    Добавил конфиги линтеров

--- kolesa-shop/.eslintrc.js
+++ kolesa-shop/.eslintrc.js
@@ -1,17 +1,85 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
+    root:    true,
+    extends: 'airbnb-base',
+    env:     {
+        browser: true,
+    },
+    rules: {
+        'max-len':    ['error', 120, 4, { ignoreComments: true }],
+        'func-names': ['error', 'never'],
+        indent:       ['error', 4, {
+            SwitchCase:   1,
+            ignoredNodes: ['TemplateLiteral'],
+        }],
+        'key-spacing':     ['error', { align: 'value' }],
+        'no-multi-spaces': ['error', {
+            exceptions: {
+                VariableDeclarator: true,
+                ImportDeclaration:  true,
+            },
+        }],
+        'no-new':               'off',
+        'no-param-reassign':    'off',
+        'linebreak-style':      'off',
+        'no-underscore-dangle': ['error', {
+            enforceInMethodNames: false,
+            allowAfterThis:       true,
+        }],
+        'padding-line-between-statements': [
+            'error',
+            {
+                blankLine: 'always',
+                prev:      '*',
+                next:      'return',
+            },
+            {
+                blankLine: 'always',
+                prev:      [
+                    'const',
+                    'let',
+                    'var',
+                ],
+                next: '*',
+            },
+            {
+                blankLine: 'any',
+                prev:      [
+                    'const',
+                    'let',
+                    'var',
+                ],
+                next: [
+                    'const',
+                    'let',
+                    'var',
+                ],
+            },
+            {
+                blankLine: 'always',
+                prev:      'if',
+                next:      '*',
+            },
+            {
+                blankLine: 'always',
+                prev:      '*',
+                next:      'if',
+            },
+            {
+                blankLine: 'always',
+                prev:      'import',
+                next:      '*',
+            },
+            {
+                blankLine: 'any',
+                prev:      'import',
+                next:      'import',
+            },
+            {
+                blankLine: 'always',
+                prev:      '*',
+                next:      'export',
+            },
+        ],
+        'arrow-parens': ['error', 'as-needed', { requireForBlockBody: true }],
+    },
 };
